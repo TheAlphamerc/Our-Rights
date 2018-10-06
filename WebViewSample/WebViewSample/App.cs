9e169e53c5608a0753677259011320446d6d4000
Sonu Sharma <sonu.sahrma@mobiloitte.in>
2018-10-06 19:05:58 +0530

    Initial commit

---
+++ WebViewSample/WebViewSample/App.cs
@@ -0,0 +1,30 @@
+﻿using WebViewSample.XAML_Implementation;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly:XamlCompilation(XamlCompilationOptions.Compile)]
+namespace WebViewSample
+{
+    public class App : Application
+    {
+        public App()
+        {
+			MainPage = new LoadWebViewPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ WebViewSample/WebViewSample/XAML Implementation/LoadWebViewPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WebViewSample.XAML_Implementation
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoadWebViewPage : ContentPage
+	{
+		public LoadWebViewPage ()
+		{
+			InitializeComponent ();
+		}
+        /// <summary>
+		/// Called when the webview starts navigating. Displays the loading label.
+		/// </summary>
+		void webviewNavigating(object sender, WebNavigatingEventArgs e)
+        {
+            this.labelLoading.IsVisible = true; //display the label when navigating starts
+        }
+
+        /// <summary>
+        /// Called when the webview finished navigating. Hides the loading label.
+        /// </summary>
+        void webviewNavigated(object sender, WebNavigatedEventArgs e)
+        {
+            this.labelLoading.IsVisible = false; //remove the loading indicator when navigating is finished
+        }
+        protected override bool OnBackButtonPressed()
+        {
+            if (webView.CanGoBack)
+            {
+                webView.GoBack();
+            }
+
+            return true; ;
+        }
+    }
+}

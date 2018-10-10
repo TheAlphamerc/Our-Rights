dbc7cb6db4d0e0b1f4904b1be0e74c77c4b4e930
Sonu Sharma <sonu.sahrma@mobiloitte.in>
2018-10-10 19:21:46 +0530

    Add app center sdk

--- WebViewSample/WebViewSample/App.cs
+++ WebViewSample/WebViewSample/App.cs
@@ -1,4 +1,7 @@
-﻿using WebViewSample.XAML_Implementation;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using WebViewSample.XAML_Implementation;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -14,6 +17,10 @@ namespace WebViewSample
 
         protected override void OnStart()
         {
+            AppCenter.Start("android=6beb8110-6eea-4850-b78f-03326f1a952f;" +
+                  "uwp=6beb8140-6eea-4850-b78f-03326f1a952f;" +
+                  "ios=6beb8120-6eea-4850-b78f-03326f1a952f",
+                  typeof(Analytics), typeof(Crashes));
             // Handle when your app starts
         }
 
